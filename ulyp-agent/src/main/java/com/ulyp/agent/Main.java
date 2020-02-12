f939c15a09568965d60bcf681b7588d706f4143c
0xaa4eb <37665782+0xaa4eb@users.noreply.github.com>
2020-02-12 18:47:51 +0300

    Main class to print sample usage

--- ulyp-agent/src/main/java/com/ulyp/agent/Main.java
+++ ulyp-agent/src/main/java/com/ulyp/agent/Main.java
@@ -2,6 +2,8 @@ package com.ulyp.agent;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sample usage: ");
+        System.out.println("Sample usage: java -javaagent:C:\\Work\\ulyp\\ulyp-agent\\build\\libs\\ulyp-agent-0.1.jar\n " +
+                "-Dulyp.packages=com.demo,org.hibernate,org.h2\n " +
+                "-Dulyp.start-method=JpaProxyUserRepositoryIntegrationTest.sampleTestCase YourClass");
     }
 }
