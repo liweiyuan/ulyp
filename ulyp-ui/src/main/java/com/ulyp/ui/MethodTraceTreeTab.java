b8c811d6a273935cd7df943d105b4f08159c328c
0xaa4eb <37665782+0xaa4eb@users.noreply.github.com>
2020-03-03 18:45:08 +0300

    Do not keep reference to tree

--- ulyp-ui/src/main/java/com/ulyp/ui/MethodTraceTreeTab.java
+++ ulyp-ui/src/main/java/com/ulyp/ui/MethodTraceTreeTab.java
@@ -6,12 +6,10 @@ import javafx.scene.control.Tab;
 public class MethodTraceTreeTab {
 
     private final Tab tab;
-    private final MethodTraceTree methodTree;
     private final long orderStamp;
 
     public MethodTraceTreeTab(Tab tab, MethodTraceTree methodTree, long orderStamp) {
         this.tab = tab;
-        this.methodTree = methodTree;
         this.orderStamp = orderStamp;
     }
 
