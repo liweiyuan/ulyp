9763aaceb641baccdcdefd1b781e7083b881ed47
0xaa4eb <37665782+0xaa4eb@users.noreply.github.com>
2020-03-16 11:22:21 +0300

    Do not use inheritance for context

--- ulyp-agent/src/main/java/com/ulyp/agent/ProgramContext.java
+++ ulyp-agent/src/main/java/com/ulyp/agent/ProgramContext.java
@@ -2,15 +2,55 @@ package com.ulyp.agent;
 
 import com.ulyp.agent.transport.UploadingTransport;
 import com.ulyp.agent.util.Log;
+import com.ulyp.agent.util.NoopLog;
+import com.ulyp.agent.util.ProcessUtils;
+import com.ulyp.agent.util.SysoutLog;
+import com.ulyp.core.ProcessInfo;
 
-// TODO AgentContext
-public interface ProgramContext {
+import java.util.concurrent.TimeUnit;
 
-    Settings getSettings();
+public class ProgramContext {
 
-    MethodDescriptionDictionary getMethodCache();
+    private final Settings settings;
+    private final UploadingTransport transport;
+    private final MethodDescriptionDictionary methodCache;
+    private final Log log;
 
-    Log getLog();
+    public ProgramContext() {
+        this.settings = Settings.fromJavaProperties();
+        if (settings.loggingTurnedOn()) {
+            this.log = new SysoutLog();
+        } else {
+            this.log = new NoopLog();
+        }
+        this.methodCache = new MethodDescriptionDictionary(log);
+        this.transport = new UploadingTransport(new ProcessInfo(ProcessUtils.getMainClassName()), settings.getUiAddress());
 
-    UploadingTransport getTransport();
+        Thread shutdown = new Thread(
+                () -> {
+                    try {
+                        transport.shutdownNowAndAwaitForTraceLogsSending(5, TimeUnit.SECONDS);
+                    } catch (InterruptedException e) {
+                        // ignore
+                    }
+                }
+        );
+        Runtime.getRuntime().addShutdownHook(shutdown);
+    }
+
+    public UploadingTransport getTransport() {
+        return transport;
+    }
+
+    public Settings getSettings() {
+        return settings;
+    }
+
+    public MethodDescriptionDictionary getMethodCache() {
+        return methodCache;
+    }
+
+    public Log getLog() {
+        return log;
+    }
 }
