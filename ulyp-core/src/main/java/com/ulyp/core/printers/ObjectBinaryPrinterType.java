fc3047e6b3dd499ce5b9d80aec115da2450ae4e3
0xaa4eb <37665782+0xaa4eb@users.noreply.github.com>
2020-03-27 17:22:53 +0300

    Rarely used printers should have higher order

--- ulyp-core/src/main/java/com/ulyp/core/printers/ObjectBinaryPrinterType.java
+++ ulyp-core/src/main/java/com/ulyp/core/printers/ObjectBinaryPrinterType.java
@@ -2,12 +2,12 @@ package com.ulyp.core.printers;
 
 public enum ObjectBinaryPrinterType {
     IDENTITY_PRINTER(new IdentityPrinter(0), Integer.MAX_VALUE),
-    CLASS_OBJECT_PRINTER(new ClassObjectPrinter(1), 0),
+    CLASS_OBJECT_PRINTER(new ClassObjectPrinter(1), 20),
     STRING_PRINTER(new StringPrinter(2), 0),
     COLLECTION(new CollectionPrinter(3), 1),
     TO_STRING_PRINTER(new ToStringPrinter(4), 10),
-    THROWABLE_PRINTER(new ThrowablePrinter(5), 0),
-    ENUM_PRINTER(new EnumPrinter(6), 0);
+    THROWABLE_PRINTER(new ThrowablePrinter(5), 20),
+    ENUM_PRINTER(new EnumPrinter(6), 20);
 
     private final ObjectBinaryPrinter printer;
     private final int order;
