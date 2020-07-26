af58e23e9f4b4ea1096706eb6e7c38f8a9c4e10e
0xaa4eb <37665782+0xaa4eb@users.noreply.github.com>
2020-07-26 16:48:07 +0300

    Actualize main class

--- ulyp-agent/src/main/java/com/ulyp/agent/Main.java
+++ ulyp-agent/src/main/java/com/ulyp/agent/Main.java
@@ -2,9 +2,6 @@ package com.ulyp.agent;
 
 public class Main {
     public static void main(String[] args) {
-        // TODO replace
-        System.out.println("Sample usage: java -javaagent:C:\\Work\\ulyp\\ulyp-agent\\build\\libs\\ulyp-agent-0.1.jar\n " +
-                "-Dulyp.packages=com.demo,org.hibernate,org.h2\n " +
-                "-Dulyp.start-method=JpaProxyUserRepositoryIntegrationTest.sampleTestCase YourClass");
+        System.out.println("Sample usage: java -javaagent:C:\\Work\\ulyp\\ulyp-agent\\build\\libs\\ulyp-agent-0.1.jar YourClass\n");
     }
 }
