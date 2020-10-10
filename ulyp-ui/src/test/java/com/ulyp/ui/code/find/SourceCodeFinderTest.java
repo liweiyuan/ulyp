073e9fa81d1a296b254540d9237cfb80a4247621
0xaa4eb <37665782+0xaa4eb@users.noreply.github.com>
2020-10-10 12:50:40 +0300

    [UI] Test for byte code + decompile source code search

--- ulyp-ui/src/test/java/com/ulyp/ui/code/find/SourceCodeFinderTest.java
+++ ulyp-ui/src/test/java/com/ulyp/ui/code/find/SourceCodeFinderTest.java
@@ -2,7 +2,12 @@ package com.ulyp.ui.code.find;
 
 import com.ulyp.core.process.Classpath;
 import com.ulyp.ui.code.SourceCode;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
@@ -10,10 +15,10 @@ import static org.junit.Assert.assertThat;
 
 public class SourceCodeFinderTest {
 
-    private final SourceCodeFinder sourceCodeFinder = new SourceCodeFinder(new Classpath().toList());
-
     @Test
     public void shouldFindSourceCodeFromJunitLibraryInCurrentClasspath() {
+        SourceCodeFinder sourceCodeFinder = new SourceCodeFinder(new Classpath().toList());
+
         SourceCode sourceCode = sourceCodeFinder.find("org.junit.Test");
 
 
@@ -21,5 +26,17 @@ public class SourceCodeFinderTest {
         assertThat(sourceCode.getCode(), containsString("public @interface Test {"));
 
         assertThat(sourceCode.getClassName(), is("org.junit.Test"));
+    }
+
+    @Test
+    public void shouldFindBytecodeAndDecompile() {
+        SourceCodeFinder sourceCodeFinder = new SourceCodeFinder(Arrays.asList(Paths.get("src", "test", "resources", "ProcessTab.jar").toString()));
+
+        SourceCode sourceCode = sourceCodeFinder.find("com.ulyp.ui.ProcessTab");
+
+        Assert.assertThat(sourceCode.getCode(), Matchers.containsString("// Decompiled from"));
+        Assert.assertThat(sourceCode.getCode(), Matchers.containsString("import org.springframework.stereotype.Component;"));
+        Assert.assertThat(sourceCode.getCode(), Matchers.containsString("@Component"));
+        Assert.assertThat(sourceCode.getCode(), Matchers.containsString("public class ProcessTab extends Tab {"));
     }
 }
