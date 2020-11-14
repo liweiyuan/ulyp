23423e1c371eafdf0a76d8069a254d839d133b85
0xaa4eb <37665782+0xaa4eb@users.noreply.github.com>
2020-11-14 21:18:04 +0300

    Better comments

--- ulyp-ui-repository/src/main/java/com/ulyp/core/CallRecord.java
+++ ulyp-ui-repository/src/main/java/com/ulyp/core/CallRecord.java
@@ -10,6 +10,28 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
+/**
+ * Method call record which was deserialized from binary format into POJO. Stands for a particular
+ * method call in some recording session.
+ *
+ * Call records are managed as tree nodes. Let's say there is the following method being recorded:
+ *
+ * public int a() {
+ *     b();
+ *     c();
+ *     return 2;
+ * }
+ *
+ * If methods b() and c() do not have any method calls in them, then there should be 3 call record nodes in
+ * total. The root is a() call which have children call records for b() and c().
+ *
+ * Every call record node has unique (within recording session) identifier which always starts from 0.
+ * Thus a() call record will have id 0, b() will have 1, etc
+ *
+ * Sometimes call records may not be complete, in that case it's not possible to know, what method returned
+ * and whether it thrown any exception or not. This happens when method exit has not yet been recorded (i.e.
+ * method exit still hasn't happened or app crashed during recording session)
+ */
 public class CallRecord {
 
     private final long id;
--- ulyp-ui-repository/src/main/java/com/ulyp/core/CallRecordDatabase.java
+++ ulyp-ui-repository/src/main/java/com/ulyp/core/CallRecordDatabase.java
@@ -4,8 +4,15 @@ import it.unimi.dsi.fastutil.longs.LongList;
 
 import java.util.List;
 
+/**
+ * Database which manages a particular call record tree.
+ */
 public interface CallRecordDatabase {
 
+    /**
+    * @return the root of the call record tree. The root node stands for the first method which is called in a
+    * recording session.
+    */
     default CallRecord getRoot() {
         return find(0);
     }
@@ -18,6 +25,9 @@ public interface CallRecordDatabase {
 
     LongList getChildrenIds(long id);
 
+    /**
+     * @return total count of call records in the tree
+     */
     long countAll();
 
     long getSubtreeCount(long id);
