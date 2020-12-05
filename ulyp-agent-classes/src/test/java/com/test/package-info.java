866d2b69ab6a646e987dbabf8bf532643f081754
0xaa4eb <37665782+0xaa4eb@users.noreply.github.com>
2020-12-05 11:54:52 +0300

    Fix typo

--- ulyp-agent-classes/src/test/java/com/test/package-info.java
+++ ulyp-agent-classes/src/test/java/com/test/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Functional tests for ulyp agent. Package is com.test becuase com.ulyp package is
+ * Functional tests for ulyp agent. Package is com.test because com.ulyp package is
  * never instrumented
  */
 package com.test;
