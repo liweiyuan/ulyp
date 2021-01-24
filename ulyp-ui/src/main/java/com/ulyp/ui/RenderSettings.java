fc86732a3b22a9a935ebc3cbbc4682bf13bda997
0xaa4eb <37665782+0xaa4eb@users.noreply.github.com>
2021-01-24 18:28:08 +0300

    Show return value type on Shift key pressed

--- ulyp-ui/src/main/java/com/ulyp/ui/RenderSettings.java
+++ ulyp-ui/src/main/java/com/ulyp/ui/RenderSettings.java
@@ -1,5 +1,6 @@
 package com.ulyp.ui;
 
+import com.google.common.base.Preconditions;
 import javafx.application.Platform;
 import org.springframework.stereotype.Component;
 
@@ -9,12 +10,12 @@ public class RenderSettings {
     private boolean showTypes = false;
 
     public boolean showTypes() {
-        Platform.isFxApplicationThread();
+        Preconditions.checkState(Platform.isFxApplicationThread(), "Not in FX application thread");
         return showTypes;
     }
 
     public RenderSettings setShowTypes(boolean showTypes) {
-        Platform.isFxApplicationThread();
+        Preconditions.checkState(Platform.isFxApplicationThread(), "Not in FX application thread");
         this.showTypes = showTypes;
         return this;
     }
