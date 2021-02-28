a615c33bf8632e08af28f179684a6faad6dcc406
0xaa4eb <37665782+0xaa4eb@users.noreply.github.com>
2021-02-28 17:45:09 +0300

    Cleanup

--- ulyp-agent/src/main/java/com/test/A.java
+++
@@ -1,8 +0,0 @@
-package com.test;
-
-public class A {
-
-    public A() {
-        System.out.println("a");
-    }
-}
--- ulyp-agent/src/main/java/com/test/B.java
+++
@@ -1,8 +0,0 @@
-package com.test;
-
-public class B {
-
-    public static void foo() {
-        System.out.println("z");
-    }
-}
