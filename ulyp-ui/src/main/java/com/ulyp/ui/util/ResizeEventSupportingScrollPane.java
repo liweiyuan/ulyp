4f0bddd1e00803df4b3896edb98c46f266008c58
0xaa4eb <37665782+0xaa4eb@users.noreply.github.com>
2021-02-28 17:25:56 +0300

    Resize on "=" and "-" keys

--- ulyp-ui/src/main/java/com/ulyp/ui/util/ResizeEventSupportingScrollPane.java
+++ ulyp-ui/src/main/java/com/ulyp/ui/util/ResizeEventSupportingScrollPane.java
@@ -10,62 +10,20 @@ import java.util.List;
 public class ResizeEventSupportingScrollPane extends ScrollPane {
 
     private final List<ResizeEventListener> listeners = new ArrayList<>();
-    private boolean ctrlPressed = false;
 
     public ResizeEventSupportingScrollPane(Node content) {
         super(content);
 
         this.setOnKeyPressed(
                 key -> {
-                    if (key.getCode() == KeyCode.CONTROL) {
-                        ctrlPressed = true;
-                    }
-                }
-        );
-
-        this.setOnKeyReleased(
-                key -> {
-                    if (key.getCode() == KeyCode.CONTROL) {
-                        ctrlPressed = false;
-                    }
-                }
-        );
-
-        this.vvalueProperty().addListener(
-                (observable, oldValue, newValue) -> {
-                    if (!ctrlPressed) {
-                        return;
-                    }
-
-                    double oldDouble = oldValue.doubleValue();
-                    double newDouble = newValue.doubleValue();
-                    if (oldDouble == 0.0 && newDouble == 0.0) {
-                        return;
-                    }
-                    if (oldDouble == 1.0 && newDouble == 1.0) {
-                        return;
-                    }
-
-                    double delta = newDouble - oldDouble;
-                    if (delta > 0.0) {
-                        listeners.forEach(listener -> listener.onResizeRequested(ResizeEvent.DOWN));
-                    } else if (delta < 0.0) {
+                    if (key.getCode() == KeyCode.EQUALS) {
                         listeners.forEach(listener -> listener.onResizeRequested(ResizeEvent.UP));
                     }
+                    if (key.getCode() == KeyCode.MINUS) {
+                        listeners.forEach(listener -> listener.onResizeRequested(ResizeEvent.DOWN));
+                    }
                 }
         );
-
-        this.setOnScroll(ev -> {
-            if (!ctrlPressed) {
-                return;
-            }
-
-            if (ev.getDeltaY() > 0.0) {
-                listeners.forEach(listener -> listener.onResizeRequested(ResizeEvent.UP));
-            } else {
-                listeners.forEach(listener -> listener.onResizeRequested(ResizeEvent.DOWN));
-            }
-        });
     }
 
     public void addListener(ResizeEventListener listener) {
